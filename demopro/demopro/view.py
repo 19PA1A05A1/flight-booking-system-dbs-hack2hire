9e81357398dc324d396a54b04ac2f4aaa8dfff82
JogiNagamalleswararao <83440222+JogiNagamalleswararao@users.noreply.github.com>
2022-10-29 12:52:23 +0530

    Add files via upload

---
+++ demopro/demopro/view.py
@@ -0,0 +1,8 @@
+"""from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("hi")
+def demo(request):
+    return HttpResponse("my name is malli")"""
